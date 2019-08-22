591b4eeb083ccd5751e684e02ed3f406f123589e
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 19:59:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require "pry"
 def oxford_comma(array)
-
+  if array.count <= 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+   x = ", and #{array.pop}"
+   array.join(", ") + x
+  end
 end
