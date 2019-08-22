62b56b1f10608256c6c5d89eaad835ee937cda90
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 19:58:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,11 +7,5 @@ def oxford_comma(array)
   else
    x = ", and #{array.pop}"
    array.join(", ") + x
-end
-#   binding.pry 
-#   fruit = []
-#   [].join
-# p fruit
-# fruit = ["kiwi", "strawberries"].join.insert(4, " and ")
-# p fruit
+  end
 end
