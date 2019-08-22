eab8387af09352625caaf8e1a4d2ddb9cd150258
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 18:30:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
 def oxford_comma(string_elements)
-  value = ["spaghetti", "meatballs", "salad"].join(", ").insert(2, "and")
+  value = ["spaghetti", "meatballs", "salad"].join(", ")
+  value.insert(2, "and")
 end
