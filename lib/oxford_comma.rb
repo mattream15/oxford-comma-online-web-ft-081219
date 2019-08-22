7aaf7a899469631409da34169b70393529d163a1
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 19:05:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(fruit)
   single = ["kiwi"].join
 p single
 end
-def oxford_comma(fruits)
-double = ["kiwi", "strawberries"].join.insert(4, " and ")
-p double
-end
+#def oxford_comma(fruits)
+#double = ["kiwi", "strawberries"].join.insert(4, " and ")
+#p double
+#end
