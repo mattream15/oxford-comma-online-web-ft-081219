32c1efe54706aeab1629bb5ab8df947333033727
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 18:56:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(string_elements)
   single = ["kiwi"].join
 p single
 end
-double = ["kiwi", "strawberries"].join.insert(4, " and ")
-p double
-end
+#double = ["kiwi", "strawberries"].join.insert(4, " and ")
+#p double
+#end
